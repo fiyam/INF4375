f9ad696e95613ce213e1e6620bbd7996bb8f91fc
mchamseddine <mchamseddine@WS-1038.HYPERTEC-GROUP.com>
2018-10-12 13:46:32 -0400

    first commit

---
+++ src/ca/uqam/SmartHouse.java
@@ -0,0 +1,28 @@
+package ca.uqam;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class SmartHouse {
+	
+	public static void main(String [] args) {
+		
+		ZMQ.Socket publisher;
+		ZMQ.Socket subscriber;
+		
+		try (ZContext context = new ZContext()) {
+			
+			// Votre code ici
+			
+			// Exemples
+			// https://github.com/zeromq/jeromq
+			// https://github.com/zeromq/jeromq/blob/master/src/test/java/guide/pathopub.java
+			// https://github.com/zeromq/jeromq/blob/master/src/test/java/guide/pathosub.java
+		
+                        
+                        
+                        
+                        
+        }
+	}
+}
