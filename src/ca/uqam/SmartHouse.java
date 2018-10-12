fd218c8622b28a537bbccd3bf5f1c3499c293f10
user <user@172.29.85.206>
2018-10-12 19:54:49 -0400

    adding sub and pub  with error

--- src/ca/uqam/SmartHouse.java
+++ src/ca/uqam/SmartHouse.java
@@ -22,16 +22,28 @@ public class SmartHouse {
                         
 		try (ZContext context = new ZContext()) {
 			
+                    
+                    
+                    
+                    
 
                         Socket subscriber = context.createSocket(SocketType.SUB); 
                         if (args.length == 1) 
                             subscriber.connect(args[0]); 
                         else subscriber.connect("tcp://localhost:5555"); 
+                        
+                          Socket publisher = context.createSocket(SocketType.PUB); 
+                       if (args.length == 1) 
+                           publisher.connect(args[0]); 
+                       else publisher.bind("tcp://*:6666"); 
+                       
+                       
+                       Thread.sleep(1000); 
 
-
-                        Random rand = new Random(System.currentTimeMillis()); 
-                        String subscription = String.format("%03d", rand.nextInt(1000)); 
-                        subscriber.subscribe(subscription.getBytes(ZMQ.CHARSET)); 
+                        subscriber.subscribe("temperature"); 
+                        subscriber.subscribe("activity"); 
+                        subscriber.subscribe("time"); 
+                        subscriber.subscribe("door_lock_sensor"); 
 
 
                         while (true) { 
@@ -39,53 +51,37 @@ public class SmartHouse {
                            if (topic == null) 
                                 break; 
                             String data = subscriber.recvStr(); 
-                            assert (topic.equals(subscription)); 
-                            System.out.println(data); 
+                            
+                             if( topic == "temperature")  {
+                             
+                                 if( data  <  19){
+                                 publisher.send("heater", ZMQ.SNDMORE); 
+                                  System.out.println(topic + " -> " + data); 
+                        
+                                    }
+                                  if( data  > 23 )){
+                                 publisher.send("ac", ZMQ.SNDMORE); 
+                                  System.out.println(topic + " -> " + data); 
+                                    }
+                             }
+                            
+                             
+                             
+                             
+                            System.out.println(topic + " -> " + data); 
+                            
+
+                            publisher.send("lights", ZMQ.SNDMORE); 
+                            publisher.send("door_lock", ZMQ.SNDMORE); 
+                            
+                            
+                           
+                            
+                            
+                            publisher.send("on"); 
                        }
 
-     
-              
-              
-              
-              
-
-
-                        Socket publisher = context.createSocket(SocketType.PUB); 
-                       if (args.length == 1) 
-                           publisher.connect(args[0]); 
-                       else publisher.bind("tcp://*:6666"); 
-
-                       //  Ensure subscriber connection has time to complete 
-                       Thread.sleep(1000); 
-
-
-                       //  Send out all 1,000 topic messages 
-                        boolean[] topics = {heater,ac,lights,door_lock} ;
-
-                       int topicNbr; 
-                       for (topicNbr = 1; topicNbr <= 4; topicNbr++) { 
-                           publisher.send(String.format("%03d", topics[topicNbr]), ZMQ.SNDMORE); 
-                           publisher.send("Save Roger"); 
-                       } 
-                       //  Send one random update per second 
-                       while (!Thread.currentThread().isInterrupted()) { 
-                           Thread.sleep(1000); 
-                           publisher.send( 
-                               String.format("%03d", rand.nextInt(1000)), ZMQ.SNDMORE 
-                           ); 
-                           publisher.send("Off with his head!"); 
-                       } 
-
-
-
-             
-              
-              
-              
-                        
-                        
-                        
-                        
+                   
         
 	}
     
